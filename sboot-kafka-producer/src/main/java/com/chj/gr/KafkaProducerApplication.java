0d851fe30c0e33bde75c89e2bb323da24572f488
jihedchaabane <jihed.chaabanee@gmail.com>
2026-01-04 21:30:24 +0100

    change bootstrap server to 192.168.1.27

--- sboot-kafka-producer/src/main/java/com/chj/gr/KafkaProducerApplication.java
+++ sboot-kafka-producer/src/main/java/com/chj/gr/KafkaProducerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaProducerApplication {
 	
 	public static void main(String[] args) {
+
 		SpringApplication.run(KafkaProducerApplication.class, args);
 	}
 }
